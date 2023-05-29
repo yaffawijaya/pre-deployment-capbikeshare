851639e4e8ab47afbc88e43fff467a20b73c4cca
yaffawijaya <yaffazka@gmail.com>
2023-05-29 22:58:07 +0700

    anjay

---
+++ adnsjads.py
@@ -0,0 +1,16 @@
+
+import google.auth
+from google.cloud import storage
+import pandas as pd
+
+
+#custom function to read data in json file
+def get_csv_gcs(bucket_name, file_name):
+    csv_data = pd.read_csv('gs://' + bucket_name + '/' + file_name, encoding='utf-8')  
+    # csv_data = pd.read_excel('gs://' + bucket_name + '/' + file_name, encoding='utf-8')    
+    return csv_data
+
+bucket_name = "dataprep-staging-76a4eba9-abb7-41ce-9168-df23035f64aa"
+file_name = "Join Table.csv"
+csv_data = get_csv_gcs(bucket_name, file_name)
+print(csv_data.head(5))
