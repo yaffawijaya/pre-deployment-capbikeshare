5f4186974415aa820872e60d7a60f7c9957df490
yaffawijaya <yaffazka@gmail.com>
2023-05-29 23:01:36 +0700

    awdhadhu

--- adnsjads.py
+++ adnsjads.py
@@ -10,7 +10,7 @@ def get_csv_gcs(bucket_name, file_name):
     # csv_data = pd.read_excel('gs://' + bucket_name + '/' + file_name, encoding='utf-8')    
     return csv_data
 
-bucket_name = "dataprep-staging-76a4eba9-abb7-41ce-9168-df23035f64aa"
+bucket_name = "dataprep-staging-76a4eba9-abb7-41ce-9168-df23035f64aa/yaffazka@gmail.com/jobrun"
 file_name = "Join Table.csv"
 csv_data = get_csv_gcs(bucket_name, file_name)
 print(csv_data.head(5))
